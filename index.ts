0d6d0d858a8c75d7b39759a8867261bc5db265d8
Nguyễn Khắc Vũ <chodiem.io@gmail.com>
2022-07-21 15:00:06 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,11 +2,10 @@ import express, { Express, Request, Response } from "express";
 import config from "./src/config/config";
 import { ConnectDB } from "./src/config/mongo";
 import router from "./src/routes";
-import bodyParser from 'body-parser';
-import cors from 'cors'
+import bodyParser from "body-parser";
+import cors from "cors";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
-
 
 const app: Express = express();
 
@@ -36,11 +35,11 @@ const MONGO_URL = `mongodb+srv://${username}:${password}@phimmoi.2rsmb.mongodb.n
 const main = async () => {
   await ConnectDB(MONGO_URL);
 
-  app.use(morgan('dev'))
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+  app.use(morgan("dev"));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
   app.use(cors());
-  // app.use("/docs-api", 
+  // app.use("/docs-api",
   //   swaggerUi.serve,
   //   swaggerUi.setup(undefined, {
   //     swaggerOptions: {
@@ -48,7 +47,7 @@ const main = async () => {
   //     },
   //   })
   // )
-  app.set('trust proxy', 1)
+  app.set("trust proxy", 1);
 
   router(app);
   app.listen(PORT, () => {
